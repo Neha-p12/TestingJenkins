9debaec2f45ff8a92109a70cdcfdfb32d965c537
Neha-p12 <nehapawara2779@gmail.com>
2024-02-11 06:08:00 +0530

    update

--- NewProj/src/demo/Main.java
+++ NewProj/src/demo/Main.java
@@ -3,7 +3,8 @@ package demo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Main ");
+		System.out.println(" Hello ");
+		System.out.println(" Main ");
 
 	}
 
